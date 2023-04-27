ce0f90d2fede963f456d93eaef55c522700a6d4d
Genife <GenifeUA@artemzheglov10@gmail.com>
2023-04-27 18:42:11 +0300

    Небольшой фикс проигрывания звуков

--- src/main/java/com/genife/adventbombs/SoundUtils/CreateSound.java
+++ src/main/java/com/genife/adventbombs/SoundUtils/CreateSound.java
@@ -1,6 +1,5 @@
 package com.genife.adventbombs.SoundUtils;
 
-import com.genife.adventbombs.AdventBombs;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
@@ -8,10 +7,10 @@ import java.util.Collection;
 
 public class CreateSound {
 
-    // функция для проигрывания звука в определенном радиусе,
+    // Функция для проигрывания звука в определенном радиусе,
     // с уникальной для каждого громкостью, на нужной локации
     public CreateSound(String soundName, int range, Location location) {
         Collection<Player> players = location.getWorld().getNearbyPlayers(location, range);
-        new PlaySound(AdventBombs.getInstance(), location, range, soundName, players.toArray(new Player[0]));
+        new PlaySound(location, range, soundName, players.toArray(new Player[0]));
     }
 }
--- src/main/java/com/genife/adventbombs/SoundUtils/PlaySound.java
+++ src/main/java/com/genife/adventbombs/SoundUtils/PlaySound.java
@@ -2,38 +2,18 @@ package com.genife.adventbombs.SoundUtils;
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+public class PlaySound {
 
-public class PlaySound extends BukkitRunnable {
-    private final int range;
-    private final Location music;
-    private final String sound;
-    private final Set<Player> players = new HashSet<>();
-
-    public PlaySound(JavaPlugin plugin, Location playLocation, int playRange, String soundName, Player[] targets) {
-        this.music = playLocation;
-        this.range = playRange;
-        this.sound = soundName;
-
-        players.addAll(Arrays.asList(targets));
-
-        runTask(plugin);
+    public PlaySound(Location playLocation, int playRange, String soundName, Player[] targets) {
+        for (Player target : targets) {
+            target.playSound(target.getLocation(), soundName, convertForSound((float) target.getLocation().distance(playLocation), playRange), 1);
+            System.out.println(target.getLocation().distance(playLocation));
+        }
     }
 
     // вычисляет нужную громкость
-    public static float convertForSound(float distance, int range) {
+    private static float convertForSound(float distance, int range) {
         return Math.max(0, 1 - (distance / range));
-    }
-
-    @Override
-    public void run() {
-        for (Player target : players) {
-            target.playSound(target.getLocation(), sound, convertForSound((float) target.getLocation().distance(music), range), 1);
-        }
     }
 }
